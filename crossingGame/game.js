18647a8b332d007cb34b15b750207410e40b3a22
Richard <40692203+richard-hutchings@users.noreply.github.com>
2019-10-29 19:04:06 +1100

    initial commit

---
+++ crossingGame/game.js
@@ -0,0 +1,25 @@
+
+/* let screenWidth = 1320;
+ let screenHeight = 520;
+
+ class GameCharacter {
+     constructor ( x, y, width, height, color) {
+         this.x = x;
+         this.y = y;
+         this.width = width;
+         this.height = height;
+         this.color = color;
+     }
+ }
+
+var rectangle = new GameCharacter(50, 70, 50, 50, 'rgb(0, 255, 0)' );
+
+var draw = function () {
+    ctx.clearRect(0, 0, screenWidth, screenHeight);
+    ctx.fillStyle = rectangle.color;
+    ctx.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height)
+}
+
+ var step = () => {
+     window.requestAnimationFrame(step);
+ }  */
